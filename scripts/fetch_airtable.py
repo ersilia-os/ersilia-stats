77c6ac1d4e371eb7e044c91ed2b3a7bf12df1751
itsjackfan <fieryphoenix425@gmail.com>
2024-11-10 14:02:32 -0500

    Fetching the Airtable into a CSV for ease of testing

--- scripts/fetch_airtable.py
+++ scripts/fetch_airtable.py
@@ -106,10 +106,7 @@ def main():
     args = parse_arguments()
 
     # Obtain Airtable API key from environment variable
-    airtable_api_key = os.getenv('AIRTABLE_API_KEY')
-    if not airtable_api_key:
-        print("Error: AIRTABLE_API_KEY environment variable is not set.")
-        sys.exit(1)
+    airtable_api_key = sys.argv[1]
 
     # Fetch data from Airtable
     records = fetch_table(airtable_api_key, args.base_id, args.table_id)
